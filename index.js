9243e7ca6e5e92756ad0a5f9cdf869949b35b78b
Asha <asha.y.dahir@gmail.com>
2022-09-08 16:23:31 +0300

    finished

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// function Submit(){
+//     console.log ("button")
+// }
+// function reset(){
+//     console.log("button")
+// }
+// let myBtn = document.getElementById("button");
+// document.addEventListener("click", "submit");
+// let myBtn = document.getElementById("button");
+// document.addEventListener("click", "rest");
+// function showDiv() {
+//     document.getElementById("target").innerHTML = 
+//     function showforms(){
+//       form.style.visibility= 'show'
+//     }
+//     function hideforms(){
+//         form.style.visibility= 'hidden'
+//     }
+//     function button(){
+//         console.log('Apply pressed')
+//     }
+    
+
+//        function myFunction() {
+//     document.getElementById("button").reset("");
+
+//   }
+// //       function replaceContentInOtherContainer(replace_target, source) {
+//         document.getElementById(replace_target).innerHTML = document.getElementById(source).innerHTML;
+//       }
+//       function book() {
+//         console.log("button")
+//       }
+//     }
+    function showDiv(){
+document.getElementById("target").removeAttribute("hidden")
+    }
+    // function myBtn() {
+    //     document.getElementById("button").()
+        
+    // }
+    // function button() {
+    //     document.getElementById("button").ATTRIBUTE_NODE()
+        
+    // }
+//     let myBtn = document.getElementById("button");
+// document.addEventListener("click", "submit");
+// let myBtn = document.getElementById("button");
+// document.addEventListener("click", "rest");
+function myFunction() {
+    document.getElementById("button").reset("");
+
+  }
+  function myBtn() {
+    document.getElementById("button").submit("");
+
+  }
+  module.exports = () => {
+    const data = { users: [] }
+    // Create 1000 users
+    for (let i = 0; i < 1000; i++) {
+      data.users.push({ id: i, name: `user${i}` })
+    }
+    return data
+  }
